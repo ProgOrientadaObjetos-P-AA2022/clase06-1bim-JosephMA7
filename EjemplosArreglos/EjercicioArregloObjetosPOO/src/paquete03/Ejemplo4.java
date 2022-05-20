48a3844626ac62ef7ee9bb4983ca7ce0678f485c
JosephMA7 <jjmartinez10@utpl.edu.ec>
2022-05-20 11:45:24 -0500

    Clase06

--- EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/Ejemplo4.java
+++ EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/Ejemplo4.java
@@ -3,7 +3,7 @@ package paquete03;
 
 import paquete01.Calificacion;
 
-public class Ejemplo {
+public class Ejemplo4 {
     public static void main(String[] args) {
         // crear un arreglo de objetos de tipo Calificacion
         Calificacion [] calificaciones = new Calificacion[3];
@@ -16,13 +16,20 @@ public class Ejemplo {
         calificaciones[1] = c2;
         calificaciones[2] = c3;
         
+        double suma =0;
         
         for (int i = 0; i < calificaciones.length; i++) {
             Calificacion objetoCalificacion = calificaciones[i];
-            System.out.printf("%s - %.2f\n", 
-                    objetoCalificacion.obtenerNombreMateria(),
-                    objetoCalificacion.obtenerNota());
+            suma = suma + objetoCalificacion.obtenerNota();
         }
+        double promedio = suma/calificaciones.length;
+        System.out.printf("%4f\n", promedio);
+          
         
+        for (int i = 0; i < calificaciones.length; i++) {
+            Calificacion objetoCalificacion = calificaciones[i];
+            suma = suma + objetoCalificacion.obtenerNota();
+            System.out.printf("%s\n",objetoCalificacion.obtenerNombreMateria());
+        }
     }
 }
--- EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploTres.java
+++ EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploTres.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package paquete03;
 
 import paquete01.Calificacion;
@@ -11,8 +7,10 @@ public class EjemploTres {
     public static void main(String[] args) {
         // crear un arreglo de objetos de tipo Calificacion
         Calificacion [] calificaciones = {new Calificacion(10, "Computación"), 
+            new Calificacion(9,"Electrónica"),
+            new Calificacion(10,"Electrónica"),
             new Calificacion(9, "Electrónica")};
-        
+           
         for (int i = 0; i < calificaciones.length; i++) {
             Calificacion objetoCalificacion = calificaciones[i];
             System.out.printf("%s - %.2f\n", 
